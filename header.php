1bd81fd1a6ad66a30274579e64326ecfea2e863b
IsaiasNascimentoCaetano <isaias022127@hotmail.com>
2015-02-06 20:43:46 -0200

    First day

---
+++ header.php
@@ -0,0 +1,9 @@
+<?php
+
+	//Logo
+	echo '<header>';
+	echo '<img src="images/Logo.png" id="logo"/>';
+	echo '<img src="images/Arms.png" id="arms">';
+	echo '</header>';
+
+?>
--- index.php
+++ index.php
@@ -9,6 +9,38 @@
 
 </head>
 
-<body></body>
+<body>
+	<div id="father">
+
+	<?php
+
+		//Include the header
+		include("header.php");
+
+	?>
+	
+	<!--Menu-->
+	<div id="menu">
+		<ul id="list" type="none">
+
+			<li><h2><a href=#>Lista de alunos</a><h2></li>
+			<li><h2><a href=#>Notas</a><h2></li>
+			<li><h2><a href=#>Inserir novos alunos</a><h2></li>
+			<li><h2><a href=#>Inserir notas</a><h2></li>
+			<li><h2><a href=#>Logar</a><h2></li>		
+		</ul>
+	</div>
+
+	<!--Information-->
+	<div id="information">
+
+		<h1>Informações</h1>
+		<p>Este é um teste para sistema escolar</p>		
+		
+	</div>
+	
+	</div>
+
+</body>
 
 </html>
