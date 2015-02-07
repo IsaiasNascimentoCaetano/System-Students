82f6d9b9012e658e40d78a464e71bcb050872b54
IsaiasNascimentoCaetano <isaias022127@hotmail.com>
2015-02-07 15:40:58 -0200

    Small modification

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 	//Logo
 	echo '<header>';
 	echo '<img src="images/Logo.png" id="logo"/>';
-	echo '<img src="images/Arms.png" id="arms">';
+	echo '<img src="images/Arms.png" id="arms"/>';
 	echo '</header>';
 
 ?>
