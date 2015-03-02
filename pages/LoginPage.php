e1333c03e02cb93d89d069be99ec883e501c84e5
IsaiasNascimentoCaetano <isaias022127@hotmail.com>
2015-03-01 23:59:25 -0300

    Simple modifications

--- pages/LoginPage.php
+++ pages/LoginPage.php
@@ -4,6 +4,7 @@
 <head>
 	<meta charset="utf-8">
 	<script type="text/javascript" src="javascript/jquery-1.11.2.min.js"></script>
+	<link rel="stylesheet" href="css/logins.css">
 
 </head>
 
@@ -50,8 +51,42 @@
 		$type = $login->get_type($name, $password);
 		$_SESSION['type'] = $type;
 
-		echo $type;
+		echo '<div id="father>"';
 	
+		include("header.php");
+
+		echo '<div id="menu">';
+
+		//See the type
+		switch($_SESSION['type']){
+
+			case 1:
+				echo '<p id="insertNotes">Inserir notas</p>';
+				echo '<p id="insertTeachers">Inserir professor</p>';
+				echo '<p id="insertStudent">Inserir alunos</p>';
+				echo '<p id="insertNotes">Inserir notas</p>';
+				echo '<p id="insertCurricular>"Inserir matéria</p>';
+				echo '<p id="seeListStudent">Lista de alunos</p>';
+				echo '<p id="modifiesContent">Modificar conteúdo</p>';
+				break;					
+
+			case 2:
+				echo '<p id="seeListStudent">Lista de alunos</p>';
+				echo '<p id="insertNotes">Inserir notas</p>';
+				break;
+			
+			case 3:
+				echo '<p id="seeNotes">Ver notas</p>';
+				echo '<p id="seeListStudent">Lista de alunos';
+				break;				
+
+		}
+
+		echo '</div>';
+
+		echo '</div>';
+
+			
 	}
 
 ?>
